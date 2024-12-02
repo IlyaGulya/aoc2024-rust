7f8109007b56c69a0b51d087c40245b5538eeb2b
Ilya Gulya <ilya@gulya.me>
2024-12-02 23:13:49 +0500

    nit: use .unzip to get sequences

--- src/day1/part1/main.rs
+++ src/day1/part1/main.rs
@@ -1,26 +1,14 @@
 use std::iter::zip;
 
 fn main() {
-    let pairs =
+    let (mut first_seq, mut second_seq): (Vec<_>, Vec<_>) =
         include_str!("../input.txt")
             .lines()
             .map(|line| line.split_once("   ").unwrap())
-            .map(|(s, v)| (s.parse::<u32>().unwrap(), v.parse::<u32>().unwrap()));
-
-    let mut first_seq =
-        pairs
-            .clone()
-            .map(|(a, _)| a)
-            .collect::<Vec<u32>>();
-
+            .map(|(s, v)| (s.parse::<u32>().unwrap(), v.parse::<u32>().unwrap()))
+            .unzip();
+    
     first_seq.sort();
-
-    let mut second_seq =
-        pairs
-            .clone()
-            .map(|(_, b)| b)
-            .collect::<Vec<u32>>();
-
     second_seq.sort();
 
     let dist: u32 =
